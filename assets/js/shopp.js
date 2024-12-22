788892f1d12c8f36862fa83da34b92e73c1ffd10
Sadık Orak <muhammedsadikorak@gmail.com>
2024-12-22 14:28:03 +0300

    Project cerated. The first definitions are created which is ;current user,  isNewEntryValid function, Products and mainMenu function.

---
+++ assets/js/shopp.js
@@ -0,0 +1,121 @@
+const userNameMsg = "User Name :"
+const mainManuMsg = "Wellcome to Shopping App\nSelect an action:\n1 - Product List\n2 - Show Cart\n3 - Buy Product\n4 - Add Account Balance\n5 - Show Balance\n\nİptal - Exist";
+const choice = ["1", "2", "3", "4", "5"];
+const existing = "Exited The Application"
+const invalidEntry = "Invalid Entry";
+// const str = "";
+// const strSpace = " ";
+const cart = [];
+const accountBalance = [];
+const user = {};
+let value;
+
+
+const products = [
+  { id: 1, productName: "Samsung", price: 32000, stock: 40 },
+  { id: 2, productName: "Iphone", price: 76000, stock: 35 },
+  { id: 3, productName: "Huawei", price: 30000, stock: 13 },
+  { id: 4, productName: "Xiaomi", price: 38000, stock: 6 },
+  { id: 5, productName: "Oppo", price: 20000, stock: 30 }
+];
+
+
+function isNewEntryValid(msg, ...keys) {
+  const entry = prompt(msg);
+
+  const isvalue = keys.includes(entry);
+
+  if (!isvalue) {
+    if (entry === null) {
+      return false;
+    }
+    return isNewEntryValid(msg, ...keys);
+  }
+
+  return entry;
+}
+
+function addingUser() {
+  let userName = prompt(userNameMsg);
+
+  if (userName === null) {
+    return false;
+  }
+
+  userName = userName.trim();
+
+  if (!userName) {
+    alert(invalidEntry);
+    return addingUser();
+  }
+
+  user.name = userName;
+  return userName;
+}
+
+
+
+
+function mainManu() {
+
+  if (Object.keys(user).length === 0) {
+    if (!addingUser()) {
+      alert(existing);
+      return;
+    }
+  }
+
+  value = isNewEntryValid(mainManuMsg, ...choice);
+
+  if (!value) {
+    alert(existing);
+    return;
+  }
+
+
+  if (value === choice[0]) {
+    const productList = products.map((p, index) => `${index + 1} - Name: ${p.productName}, Price: ${p.price}, Stock:${p.stock}`);
+    alert(productList.join("\n"));
+  }
+
+  if (value === choice[1]) {
+
+  }
+
+  if (value === choice[2]) {
+
+  }
+
+  if (value === choice[3]) {
+
+  }
+
+  if (value === choice[4]) {
+
+  }
+
+
+
+
+}
+
+mainManu();
+
+
+
+
+
+// Yapılacaklar:
+// Ürün Listesi Oluşturma: Ürünlerin adı, fiyatı ve stok miktarını içeren bir ürün listesi oluşturulacaktır. Örnek ürünler Ürün A, Ürün B, vb. olacak ve her ürün için fiyat ve stok miktarı belirlenecektir.  ✅
+// Sepet Fonksiyonu: Kullanıcı, satın aldığı ürünleri sepete ekleyebilir. Sepetteki ürünlerin adı, fiyatı ve miktarı gösterilecektir.
+// Ürün Satın Alma İşlemi: Kullanıcı, ürünleri listeleyip bir ürün seçtikten sonra, kaç adet almak istediğini girecek. Stok ve bakiye kontrol edilecek, yeterli stok ve bakiye varsa ürün sepete eklenecek, stok ve bakiye güncellenecektir.
+// Bakiye Ekle:Bakiye ekleme yapılabilecek
+// Bakiyeyi Görüntüleme: Kullanıcı mevcut bakiyesini herhangi bir zamanda görüntüleyebilir.
+// Menü Seçenekleri: Ana menüde 5 seçenek olacak: ✅
+// Ürünleri listele  ✅
+// Sepeti göster
+// Ürün satın al
+// Bakiye Ekle
+// Bakiyeyi göster
+// Çıkış yap
+// Geçersiz Seçenek Kontrolü: Kullanıcı geçerli olmayan bir seçenek girerse, hata mesajı gösterilecektir.
